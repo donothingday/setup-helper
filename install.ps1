d0d652598153469b068667701c8511ff01294875
KIAN <ngegamming@gmail.com>
2026-07-05 18:41:28 +0700

    Add PowerShell script to download and run setup-helper

---
+++ install.ps1
@@ -0,0 +1,8 @@
+$url = "https://github.com/donothingday/setup-helper/releases/latest/download/setup-helper.exe"
+$out = "$env:TEMP\setup-helper.exe"
+
+Write-Host "Downloading Setup Helper..."
+Invoke-WebRequest -Uri $url -OutFile $out
+
+Write-Host "Running..."
+Start-Process -FilePath $out -Wait
